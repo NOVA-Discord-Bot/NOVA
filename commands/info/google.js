1df16160fb48ba048408d83e76f8610345bc7479
Frazix12 <irfanhossain683@gmail.com>
2021-08-06 12:34:00 +0600

    made more commands

---
+++ commands/info/google.js
@@ -0,0 +1,28 @@
+const discord = require("discord.js");
+const config = require("../../config.json");
+
+module.exports = {
+        name: 'google',
+        description: 'Search google',
+        aliases: ["google"],
+        usage: 'google',
+        category: '🔰Info',
+    run: async (bot, message, args) => {
+ 
+  if (!message.content.startsWith(config.prefix)) return;
+
+      const sentence = args.join("+")
+       let sntnce = message.content.split(' ');
+    sntnce.shift();
+    sntnce = sntnce.join(' ');
+    if (!sentence) return message.reply('**Please specify a search query.**');
+     let embed = new discord.MessageEmbed()
+      .setTitle('**You Searched Google**')
+      .setDescription(
+        `**Your Search Query:** ${sntnce}\n\n **Search Result** - [Click Here](https://www.google.com/search?q=${sentence}&oq=${sentence}&aqs=chrome.0.69i59l2j0l2j69i60j69i61l2j69i65.1147j0j7&sourceid=chrome&ie=UTF-8)`
+      )
+      .setColor('GREEN')
+      .setFooter('NOVA');
+    message.channel.send(embed);
+  }
+}
