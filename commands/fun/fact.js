bc1f87b6e7519e667b45fe4dc9e379975a400083
Frazix12 <irfanhossain683@gmail.com>
2021-08-11 20:57:41 +0600

    more

---
+++ commands/fun/fact.js
@@ -0,0 +1,14 @@
+const { MessageEmbed } = require('discord.js');
+const Random = require("srod-v2");
+
+module.exports = {
+    name: 'fact',
+    category: '👻Fun',
+    description: 'Shows some cool fact',
+    usage: 'fact',
+    aliases: [''],
+    run: async(bot, message, args) => {
+       let data = await Random.GetFact();
+       message.channel.send(data);
+    }
+};
